220017babbfd24c0175e66161f6fb4131c41cdbc
shudayo117 <syudayo117@iCloud.com>
2023-07-25 23:07:17 +0900

    合ってるかな？

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
-<?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;    
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return view('posts.index');
 });
+
